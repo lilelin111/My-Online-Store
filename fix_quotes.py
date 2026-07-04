500754fe58153b75f48e18e1294542cdbf97d611
lilelin <2712897289@qq.com>
2026-07-04 21:46:09 +0800

    clean: remove useless files (start scripts, test files, fix scripts)

--- fix_quotes.py
+++
@@ -1,4 +0,0 @@
-﻿content = open("store/store.go","r",encoding="utf-8").read()
-content = content.replace('\", \"date\" \"','\", \\\"date\\\" \"')
-open("store/store.go","w",encoding="utf-8").write(content)
-print("fixed")
--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\27128\Desktop\20260621\1"
-set PATH=C:\Program Files\Go\bin;%PATH%
-go run web/main.go
